e72680a0a54859bcbcdce9ebb7f4587bc636379b
Eduardo P <edtvports@gmail.com>
2022-05-19 22:47:20 -0300

    Primes.rs refactored Now it doesn't cheat the counter and uses a function, works faster.

--- src/primes.rs
+++ src/primes.rs
@@ -1,36 +1,31 @@
 use std::time::Instant;
 
 pub fn call(max: i64) -> i64 {
-
     let elapsed_t = Instant::now();
     
     let max_number: i64 = max;
     let mut prime_count: i64 = 0;
-    {
-        for number in 2..=max_number {
-            if number % 2 != 0 || number == 2 {
-                for divisor in 2..=number {
-                    if divisor % 2 != 0 && number % divisor == 0 || number == 2 {
-                        if divisor == number {
-                            prime_count += 1;
-                        }
-                        else  {
-                            break;
-                        }
-                    }
-                    else {
-                        continue;
-                    }
-                }
-            }
-            else {
-                continue;
+
+    for number in 2..max_number{
+        if number % 2 != 0 || number == 2{
+            if is_prime(number) == true {
+                prime_count += 1;
             }
         }
-        println!("{} numbers where counted\n {} primes where found.", max_number, prime_count);
     }
-    let elapsed_t = elapsed_t.elapsed();
-    println!("Elapsed time: {:.2?}", elapsed_t);
 
+    let elapsed_t = elapsed_t.elapsed();
+    println!("\nElapsed time: {:.2?}", elapsed_t);
+    println!("{} numbers where counted\n {} primes where found.", max_number, prime_count);
     return prime_count;
+}
+
+fn is_prime (value: i64) -> bool {
+    
+    for divisors in 2..value/2 {
+        if value % divisors == 0 {
+                return false;
+        }
+    }
+    return true;
 }
